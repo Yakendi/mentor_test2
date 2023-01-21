9bbb1a447ce5ac39f0274e05690027533ac973a4
Aslan <aslanmikalaev@gmail.com>
2023-01-21 17:03:16 +0500

    Fa
    qw
    wq
    clear
    vorites view controller setting

--- UnsplashAppCopy/UnsplashAppCopy/Controllers/FavoritesViewController.swift
+++ UnsplashAppCopy/UnsplashAppCopy/Controllers/FavoritesViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import SnapKit
 
 class FavoritesViewController: UIViewController {
-
+    
+    //MARK: - Puclic properties
+    var favoriteImages = [UIImage]()
+    
+    //MARK: - UI
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        layout.itemSize = CGSize(width: (view.frame.size.width - 40) / 3, height: (view.frame.size.width - 40) / 3)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         
-        navigationItem.title = "favorites"
+        setup()
+    }
+}
+
+//MARK: - Setup views
+private extension FavoritesViewController {
+    func setup() {
+        setupViews()
+        setupConstraints()
+    }
+    
+    func setupViews() {
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Favorites"
         navigationController?.navigationBar.prefersLargeTitles = true
+        view.addSubview(collectionView)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+//MARK: - Collection view data source and delegate
+extension FavoritesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        30
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemOrange
+        return cell
     }
 }
