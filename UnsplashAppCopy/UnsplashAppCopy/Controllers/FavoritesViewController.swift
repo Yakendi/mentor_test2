c2c5af7728bb312b48dfe583b7f9c4d90918b80c
Aslan <aslanmikalaev@gmail.com>
2023-01-21 19:53:54 +0500

    FavoritesVC changed. Added tableview instead collectionview

--- UnsplashAppCopy/UnsplashAppCopy/Controllers/FavoritesViewController.swift
+++ UnsplashAppCopy/UnsplashAppCopy/Controllers/FavoritesViewController.swift
@@ -18,13 +18,11 @@ class FavoritesViewController: UIViewController {
     var favoriteImages = [UIImage]()
     
     //MARK: - UI
-    private lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        layout.itemSize = CGSize(width: (view.frame.size.width - 40) / 3, height: (view.frame.size.width - 40) / 3)
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        return collectionView
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.rowHeight = UITableView.automaticDimension
+        return tableView
     }()
     
     //MARK: - Life cycle
@@ -46,26 +44,25 @@ private extension FavoritesViewController {
         view.backgroundColor = .systemBackground
         navigationItem.title = "Favorites"
         navigationController?.navigationBar.prefersLargeTitles = true
-        view.addSubview(collectionView)
-        collectionView.dataSource = self
-        collectionView.delegate = self
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
     func setupConstraints() {
-        collectionView.snp.makeConstraints { make in
+        tableView.snp.makeConstraints { make in
             make.edges.equalTo(view.safeAreaLayoutGuide)
         }
     }
 }
 
 //MARK: - Collection view data source and delegate
-extension FavoritesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        30
+extension FavoritesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
     }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .systemOrange
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         return cell
     }
 }
